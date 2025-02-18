2ce4a2212d92b241f823e0250d2e035eb8d5f28e
Olanrewaju Said <134246135+ol-s-cloud@users.noreply.github.com>
2025-02-17 18:00:40 -0800

    Add infrastructure package

---
+++ infrastructure/__init__.py
@@ -0,0 +1,8 @@
+# Circle Core Infrastructure
+# Version: 0.1.0
+
+"""
+Core infrastructure package for Circle Core framework.
+This package contains all the core infrastructure components needed
+for the Circle Core ecosystem.
+"""
